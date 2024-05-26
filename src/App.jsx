871da1693fa5a770f55377e6c9ef5c2321822fa4
haileemin <dr.leminhhai2003@gmail.com>
2024-05-27 00:25:18 +0700

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import "./App.css"
 import TaskForm from "./components/TaskForm";
@@ -6,14 +6,28 @@ import TaskColumn from './components/TaskColumn';
 import todoIcon from './assets/direct-hit.png'
 import doingIcon from './assets/glowing-star.png'
 import doneIcon from './assets/check-mark-button.png'
+
+const oldTask = localStorage.getItem("tasks")
+console.log(oldTask);
+
 const App = () => {
+  const [tasks, setTasks] = useState(JSON.parse(oldTask) || []);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+  }, [tasks])
+
+  const handleDelete = (taskIndex) => {
+    const newTasks = tasks.filter((task, index) => index !== taskIndex)
+    setTasks(newTasks)
+  };
   return (
     <div className='app'>
-      <TaskForm />
+      <TaskForm setTasks={setTasks} />
       <main className='app_main'>
-        <TaskColumn title="To do" icon={todoIcon} />
-        <TaskColumn title="Doing" icon={doingIcon} />
-        <TaskColumn title="Done" icon={doneIcon} />
+        <TaskColumn title="To do" icon={todoIcon} tasks={tasks} status="todo" handleDelete={handleDelete} />
+        <TaskColumn title="Doing" icon={doingIcon} tasks={tasks} status="doing" handleDelete={handleDelete} />
+        <TaskColumn title="Done" icon={doneIcon} tasks={tasks} status="done" handleDelete={handleDelete} />
       </main>
     </div>
   )
--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -4,17 +4,20 @@ import "./TaskCard.css"
 import Tag from './Tag'
 import deleteIcon from '../assets/delete.png'
 
-const TaskCard = () => {
+const TaskCard = ({ title, tags, handleDelete, index }) => {
     return (
         <article className='task_card'>
-            <p className='task_text'>This is Sample Text.</p>
+            <p className='task_text'>{title}</p>
 
             <div className='task_card_bottom_line'>
                 <div className='task_card_tags'>
-                    <Tag tagName='HTLM' />
-                    <Tag tagName='CSS' />
+                    {
+                        tags.map((tag, index) => (<Tag key={index} tagName={tag} selected />))
+                    }
                 </div>
-                <div className='task_delete'>
+                <div
+                    className='task_delete'
+                    onClick={() => handleDelete(index)}>
                     <img src={deleteIcon} className='delete_icon' alt="" />
                 </div>
             </div>
