993827a4630b0d3a5d4ba3b6d129ff3366108b8b
haileemin <dr.leminhhai2003@gmail.com>
2024-05-26 10:31:25 +0700

    secound commit

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import "./TaskForm.css"
 import Tag from './Tag'
 
 const TaskForm = () => {
+    const [task, setTask] = useState("");
+    const [status, setStatus] = useState("todo")
+
+    const handleTaskChange = (e) => {
+        setTask(e.target.value);
+    };
+
+    const handleStatusChange = (e) => {
+        setStatus(e.target.value);
+    };
+    console.log(task, status);
     return (
         <header className='app_header'>
             <form>
-                <input type="text" className='task_input' placeholder='Enter your task' />
+                <input type="text" className='task_input' placeholder='Enter your task' onChange={handleTaskChange} />
 
                 <div className='task_form_bottom_line'>
                     <div>
@@ -18,7 +29,7 @@ const TaskForm = () => {
                     </div>
 
                     <div>
-                        <select className='task_status'>
+                        <select className='task_status' onChange={handleStatusChange}>
                             <option value='todo'>To do</option>
                             <option value='doing'>Doing</option>
                             <option value='done'>Done</option>
